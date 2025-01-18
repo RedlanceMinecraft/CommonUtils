749c87974fd0a41cb6a8cf5d8dbad861f03a2e67
dima_dencep <dima_dencep@redlance.org>
2025-01-18 20:27:50 +0700

    Add query method

--- src/main/java/org/redlance/common/utils/ServerUtils.java
+++ src/main/java/org/redlance/common/utils/ServerUtils.java
@@ -7,7 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
+import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ServerUtils {
     public static InetSocketAddress getAddressFromProperties() {
@@ -25,5 +28,16 @@ public class ServerUtils {
                 prop.getProperty("server-ip", "0.0.0.0"),
                 Integer.parseInt(prop.getProperty("server-port", "25566"))
         );
+    }
+
+    public static String getQuery(String query, String key, String defaultValue) {
+        return getQuery(query).getOrDefault(key, defaultValue);
+    }
+
+    public static Map<String, String> getQuery(String query) {
+        return Stream.of(query.split("&"))
+                .map(string -> string.split("="))
+                .filter(strings -> strings.length > 1)
+                .collect(Collectors.toMap(values -> values[0], values -> values[1]));
     }
 }

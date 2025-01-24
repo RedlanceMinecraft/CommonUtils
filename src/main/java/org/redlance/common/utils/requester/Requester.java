0bd816cbab337d35254a6a7533f0ea9b52503d0d
dima_dencep <dima_dencep@redlance.org>
2025-01-24 18:07:03 +0700

    Fix http version

--- src/main/java/org/redlance/common/utils/requester/Requester.java
+++ src/main/java/org/redlance/common/utils/requester/Requester.java
@@ -27,7 +27,7 @@ public class Requester {
     public static final Methanol HTTP_CLIENT = Methanol.newBuilder()
             .executor(CommonUtils.EXECUTOR)
             .connectTimeout(Duration.ofMinutes(1))
-            .version(HttpClient.Version.HTTP_2)
+            .version(HttpClient.Version.HTTP_1_1)
             .proxy(UrlProxySelector.INSTANCE)
             .followRedirects(HttpClient.Redirect.ALWAYS)
             .cache(HttpCache.newBuilder()

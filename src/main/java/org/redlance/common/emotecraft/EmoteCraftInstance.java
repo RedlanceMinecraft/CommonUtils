0dfd3d58fb138468aa858f33ed5fd6b2a512ac8e
dima_dencep <dima_dencep@redlance.org>
2025-02-25 03:14:16 +0700

    Fix keyframes

--- src/main/java/org/redlance/common/emotecraft/EmoteCraftInstance.java
+++ src/main/java/org/redlance/common/emotecraft/EmoteCraftInstance.java
@@ -1,11 +1,14 @@
 package org.redlance.common.emotecraft;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dev.kosmx.playerAnim.core.data.KeyframeAnimation;
 import io.github.kosmx.emotes.common.CommonData;
 import io.github.kosmx.emotes.common.SerializableConfig;
 import io.github.kosmx.emotes.server.config.ConfigSerializer;
 import io.github.kosmx.emotes.server.config.Serializer;
 import org.redlance.common.CommonUtils;
+import org.redlance.common.emotecraft.serializer.FastAnimationSerializer;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -22,7 +25,13 @@ public class EmoteCraftInstance {
         }
 
         CommonData.isLoaded = true;
-        Serializer.INSTANCE = new Serializer<>(new ConfigSerializer<>(configSuppler), configClass, consumer);
+        Serializer.INSTANCE = new Serializer<>(new ConfigSerializer<>(configSuppler), configClass, consumer) {
+            @Override
+            protected Gson initializeSerializer(GsonBuilder builder) {
+                builder.registerTypeAdapter(KeyframeAnimation.class, FastAnimationSerializer.INSTANCE);
+                return super.initializeSerializer(builder);
+            }
+        };
 
         return getConfigAs();
     }

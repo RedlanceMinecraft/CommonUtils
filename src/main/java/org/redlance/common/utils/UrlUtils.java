37a7fa56f07c81e02f3b97dc87f5773cd128982f
dima_dencep <dima_dencep@redlance.org>
2025-03-26 15:47:23 +0700

    Optimize url filter

--- src/main/java/org/redlance/common/utils/UrlUtils.java
+++ src/main/java/org/redlance/common/utils/UrlUtils.java
@@ -1,17 +1,15 @@
 package org.redlance.common.utils;
 
 import org.jetbrains.annotations.Nullable;
-import org.redlance.common.CommonUtils;
 
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UrlUtils {
     private static final Pattern URL_PATTERN = Pattern.compile(
-            //         schema                          ipv4            OR        namespace                 port     path         ends
-            //   |-----------------|        |-------------------------|  |-------------------------|    |---------| |--|   |---------------|
-            "((?:[a-z0-9]{2,}:\\/\\/)?(?:(?:[0-9]{1,3}\\.){3}[0-9]{1,3}|(?:[-\\w_]{1,}\\.[a-z]{2,}?))(?::[0-9]{1,5})?.*?(?=[!\"\u00A7 \n]|$))",
+            "((?:[a-z0-9]{2,}:\\/\\/)?(?:(?:[0-9]{1,3}\\.){3}[0-9]{1,3}" +
+                    "|(?:[-\\w_]+\\.(?:gg|ms|com|me|net|org|online|edu|gov|mil|io|info|biz|co|ru|uk|de|jp|fr|au|us|ca|cn|in|es|br|it|nl|se|no|fi|mx|kr|ch|ua|pl|vn|cz|gr|at|be|ar|dk|hk|pt|nz|za|sg|my|th|tw)))" +
+                    "(?::[0-9]{1,5})?.*?(?=[!\"\u00A7 \n]|$))",
             Pattern.CASE_INSENSITIVE
     );
 
@@ -19,20 +17,20 @@ public class UrlUtils {
         return URL_PATTERN.matcher(input);
     }
 
-    public static String filterUrls(@Nullable String input, @Nullable String replace) {
-        if (input == null) {
-            return null;
-        }
+    public static String filterUrls(@Nullable String input, String replace) {
+        if (input == null) return null;
 
         Matcher matcher = matcher(input);
+        StringBuilder result = new StringBuilder();
+        int lastEnd = 0;
 
         while (matcher.find()) {
-            String url = input.substring(matcher.start(), matcher.end());
-
-            CommonUtils.LOGGER.trace("Found url {} in {}.", url, input);
-            input = input.replace(url, Objects.requireNonNullElse(replace, ""));
+            result.append(input, lastEnd, matcher.start());
+            if (replace != null) result.append(replace);
+            lastEnd = matcher.end();
         }
 
-        return input;
+        result.append(input.substring(lastEnd));
+        return result.toString();
     }
 }

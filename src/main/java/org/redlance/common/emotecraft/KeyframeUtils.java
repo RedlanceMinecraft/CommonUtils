1008014201d586a1e76ef5a33fedc233bafabdd7
dima_dencep <dima_dencep@redlance.org>
2025-03-29 16:29:18 +0700

    Oops

--- src/main/java/org/redlance/common/emotecraft/KeyframeUtils.java
+++ src/main/java/org/redlance/common/emotecraft/KeyframeUtils.java
@@ -3,7 +3,7 @@ package org.redlance.common.emotecraft;
 import dev.kosmx.playerAnim.core.data.KeyframeAnimation;
 
 public class KeyframeUtils {
-    private static boolean hasBends(KeyframeAnimation animation) {
+    public static boolean hasBends(KeyframeAnimation animation) {
         for (KeyframeAnimation.StateCollection collection : animation.getBodyParts().values()) {
             if (hasBends(collection)) {
                 return true;
@@ -12,7 +12,7 @@ public class KeyframeUtils {
         return false;
     }
 
-    private static boolean hasBends(KeyframeAnimation.StateCollection collection) {
+    public static boolean hasBends(KeyframeAnimation.StateCollection collection) {
         if (!collection.isBendable()) {
             return false;
         }
@@ -27,7 +27,7 @@ public class KeyframeUtils {
         return hasBends;
     }
 
-    private static boolean hasBends(KeyframeAnimation.StateCollection.State state) {
+    public static boolean hasBends(KeyframeAnimation.StateCollection.State state) {
         return state.isEnabled() && !state.getKeyFrames().isEmpty();
     }
 }

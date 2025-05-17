182b146e0e80a11c13e3b5e13f2ccd6ec90cdf18
dima_dencep <dima_dencep@redlance.org>
2025-05-17 18:17:46 +0700

    Update useragent

--- src/main/java/org/redlance/common/utils/requester/Requester.java
+++ src/main/java/org/redlance/common/utils/requester/Requester.java
@@ -37,7 +37,7 @@ public class Requester {
             )
             .interceptor(new CookieFixerInterceptor())
             .backendInterceptor(new CacheOverrideInterceptor())
-            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
+            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36")
             .cookieHandler(new CookieManager())
             .adapterCodec(AdapterCodec.newBuilder()
                     .decoder(GsonAdapterFactory.createDecoder(Serializer.getSerializer()))

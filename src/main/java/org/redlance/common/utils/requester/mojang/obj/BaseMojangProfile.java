ec62473d0b7fbf16205bd6f9f46ca9ec3a1d2bca
dima_dencep <dima_dencep@redlance.org>
2025-08-14 00:51:42 +0700

    Update BaseMojangProfile.java

--- src/main/java/org/redlance/common/utils/requester/mojang/obj/BaseMojangProfile.java
+++ src/main/java/org/redlance/common/utils/requester/mojang/obj/BaseMojangProfile.java
@@ -5,11 +5,20 @@ import org.redlance.common.utils.requester.mojang.MojangUtils;
 
 import java.util.UUID;
 
+@SuppressWarnings("unused")
 public record BaseMojangProfile(
         @SerializedName(value = "name", alternate = "username") String name,
         @SerializedName(value = "id", alternate = {"uuid", "minecraftUUID"}) String id,
         @SerializedName(value = "errorMessage", alternate = "error") String errorMessage
 ) {
+    public BaseMojangProfile(String name, String id) {
+        this(name, id, null);
+    }
+
+    public BaseMojangProfile(String name, UUID id) {
+        this(name, MojangUtils.toString(id));
+    }
+
     public UUID uuid() {
         if (MojangUtils.MOJANG_BROKEN_UUID_LENGTH == id().length()) {
             return MojangUtils.parseUuid(id());

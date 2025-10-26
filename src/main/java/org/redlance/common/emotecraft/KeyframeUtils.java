4cc0eb2d8641f6be7a667ae7dd0e4c4568943f24
ZigyTheBird <105124180+ZigyTheBird@users.noreply.github.com>
2025-10-26 21:27:29 +0330

    Port KeyframeUtils class to PAL

--- src/main/java/org/redlance/common/emotecraft/KeyframeUtils.java
+++ src/main/java/org/redlance/common/emotecraft/KeyframeUtils.java
@@ -2,32 +2,36 @@ package org.redlance.common.emotecraft;
 
 import com.zigythebird.playeranimcore.animation.Animation;
 import com.zigythebird.playeranimcore.animation.keyframe.BoneAnimation;
+import com.zigythebird.playeranimcore.animation.keyframe.Keyframe;
+import com.zigythebird.playeranimcore.animation.keyframe.KeyframeStack;
+
+import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 public class KeyframeUtils {
-    /*private static final List<String> KEYS = List.of(
-            "head", "body", "rightArm", "leftArm", "rightLeg", "leftLeg"
-    );*/
+    private static final List<String> KEYS = List.of(
+            "head", "body", "right_arm", "left_arm", "right_leg", "left_leg"
+    );
 
     public static boolean hasEasingArgs(Animation animation) {
-        return true;
-        /*for (KeyframeAnimation.StateCollection collection : animation.getBodyParts().values()) {
-            if (collectionToStates(collection).anyMatch(KeyframeUtils::hasEasingArgs)) {
-                return true;
-            }
-        }
-        return false;*/
-    }
-
-    /*public static boolean hasEasingArgs(KeyframeAnimation.StateCollection.State state) {
-        if (!isStateUsed(state)) return false;
-        for (KeyframeAnimation.KeyFrame frame : state.getKeyFrames()) {
-            if (frame.easingArg != null && !frame.easingArg.isNaN()) {
-                return true;
-            }
+        for (BoneAnimation bone : animation.boneAnimations().values()) {
+            if (hasEasingArgs(bone.positionKeyFrames()) || hasEasingArgs(bone.rotationKeyFrames())
+                    || hasEasingArgs(bone.scaleKeyFrames()) || hasEasingArgs(bone.bendKeyFrames())) return true;
         }
         return false;
-    }*/
+    }
+
+    public static boolean hasEasingArgs(KeyframeStack stack) {
+        return hasEasingArgs(stack.xKeyframes()) || hasEasingArgs(stack.yKeyframes()) || hasEasingArgs(stack.zKeyframes());
+    }
+
+    public static boolean hasEasingArgs(List<Keyframe> stack) {
+        for (Keyframe keyframe : stack) {
+            if (!keyframe.easingArgs().isEmpty()) return true;
+        }
+        return false;
+    }
 
     public static boolean hasBends(Animation animation) {
         for (BoneAnimation bone : animation.boneAnimations().values()) {
@@ -39,44 +43,22 @@ public class KeyframeUtils {
     }
 
     public static boolean hasScaling(Animation animation) {
-        return true;
-        /*for (KeyframeAnimation.StateCollection part : animation.getBodyParts().values()) {
-            if (!part.isScalable()) continue;
-            if (isStateUsed(part.scaleX) || isStateUsed(part.scaleY) || isStateUsed(part.scaleZ)) {
+        for (BoneAnimation bone : animation.boneAnimations().values()) {
+            if (bone.scaleKeyFrames().hasKeyframes()) {
                 return true;
             }
         }
-
-        return false;*/
+        return false;
     }
 
     public static boolean hasDynamicParts(Animation animation) {
-        return true;
-        /*for (Map.Entry<String, KeyframeAnimation.StateCollection> entry : animation.getBodyParts().entrySet()) {
+        for (Map.Entry<String, BoneAnimation> entry : animation.boneAnimations().entrySet()) {
             if (KEYS.contains(entry.getKey())) continue;
-            if (isCollectionUsed(entry.getValue())) {
+            if (entry.getValue().hasKeyframes()) {
                 return true;
             }
         }
 
-        return false;*/
+        return false;
     }
-
-    /*public static boolean isCollectionUsed(@Nullable KeyframeAnimation.StateCollection collection) {
-        if (collection == null) return false;
-        return collectionToStates(collection).anyMatch(KeyframeUtils::isStateUsed);
-    }
-
-    public static boolean isStateUsed(@Nullable KeyframeAnimation.StateCollection.State state) {
-        return state != null && !state.getKeyFrames().isEmpty() && state.isEnabled();
-    }
-
-    public static Stream<KeyframeAnimation.StateCollection.State> collectionToStates(KeyframeAnimation.StateCollection collection) {
-        return Stream.of(
-                collection.x, collection.y, collection.z,
-                collection.yaw, collection.pitch, collection.roll,
-                collection.bendDirection, collection.bend,
-                collection.scaleX, collection.scaleY, collection.scaleZ
-        );
-    }*/
 }

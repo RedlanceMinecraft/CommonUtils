da52b6c6710f1da972888ae81f0770aac2b5d872
dima_dencep <dima_dencep@redlance.org>
2025-11-28 23:12:47 +0700

    Boosty test

---
+++ src/test/java/org/redlance/common/tests/BoostyRequesterTest.java
@@ -0,0 +1,23 @@
+package org.redlance.common.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.redlance.common.utils.requester.boosty.BoostyRequester;
+import org.redlance.common.utils.requester.boosty.obj.user.BoostyUser;
+import org.redlance.common.utils.requester.boosty.obj.user.SubscribeStatus;
+
+import java.io.IOException;
+import java.util.List;
+
+@EnabledIfSystemProperty(named = "boosty.token", matches = ".*")
+public class BoostyRequesterTest {
+    private static final String TOKEN = System.getProperty("boosty.token");
+
+    @Test
+    public void testEnum() throws IOException, InterruptedException {
+        BoostyUser user = BoostyRequester.requestSubscribersChunking(List::getFirst, "dima_dencep", TOKEN, 2452741);
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals(SubscribeStatus.ACTIVE, user.status);
+    }
+}

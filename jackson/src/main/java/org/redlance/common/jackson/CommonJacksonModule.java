2ccf50343690dee857192df6bb98767c3f8da6de
dima_dencep <dima_dencep@redlance.org>
2025-12-29 06:37:13 +0700

    jackson

--- jackson/src/main/java/org/redlance/common/jackson/CommonJacksonModule.java
+++ jackson/src/main/java/org/redlance/common/jackson/CommonJacksonModule.java
@@ -1,8 +1,5 @@
 package org.redlance.common.jackson;
 
-import com.zigythebird.playeranimcore.animation.Animation;
-import org.redlance.common.jackson.animation.FastAnimationDeserializer;
-import org.redlance.common.jackson.animation.FastAnimationSerializer;
 import org.redlance.common.jackson.future.CompletableFutureDeserializer;
 import org.redlance.common.jackson.future.CompletableFutureSerializer;
 import tools.jackson.databind.module.SimpleModule;
@@ -16,9 +13,5 @@ public class CommonJacksonModule extends SimpleModule {
         // CompletableFuture
         addSerializer(CompletableFuture.class, new CompletableFutureSerializer());
         addDeserializer(CompletableFuture.class, new CompletableFutureDeserializer());
-
-        // Animation
-        addSerializer(Animation.class, FastAnimationSerializer.INSTANCE);
-        addDeserializer(Animation.class, FastAnimationDeserializer.INSTANCE);
     }
 }

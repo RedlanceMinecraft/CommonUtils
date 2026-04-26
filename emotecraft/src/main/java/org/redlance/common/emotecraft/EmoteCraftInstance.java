b3ba3c183e85fbd5a6b5ead4fedee9504298f26a
dima_dencep <dima_dencep@redlance.org>
2026-04-26 10:42:21 +0700

    Fixes

--- emotecraft/src/main/java/org/redlance/common/emotecraft/EmoteCraftInstance.java
+++ emotecraft/src/main/java/org/redlance/common/emotecraft/EmoteCraftInstance.java
@@ -3,33 +3,27 @@ package org.redlance.common.emotecraft;
 import io.github.kosmx.emotes.server.config.CommonConfig;
 import io.github.kosmx.emotes.server.config.ConfigSerializer;
 import io.github.kosmx.emotes.server.config.Serializer;
+import io.github.kosmx.emotes.server.services.InstanceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
 import java.util.function.Supplier;
 
 public class EmoteCraftInstance {
     private static final Logger LOGGER = LoggerFactory.getLogger("EmoteCraftInstance");
 
-    /*public static <T extends CommonConfig> void tryInitializeInstance(Supplier<T> configSuppler, Class<T> configClass) {
-        EmoteCraftInstance.tryInitializeInstance(configSuppler, configClass, null);
-    }*/
+    public static <T extends CommonConfig> void tryInitializeInstance(Supplier<T> configSuppler, int version, Class<T> configClass) {
+        EmoteCraftInstance.tryInitializeInstance(configSuppler, version, configClass, InstanceService.INSTANCE.getConfigPath());
+    }
 
-    public static <T extends CommonConfig> T tryInitializeInstance(Supplier<T> configSuppler, int version, Class<T> configClass) {
+    public static <T extends CommonConfig> T tryInitializeInstance(Supplier<T> configSuppler, int version, Class<T> configClass, Path configPath) {
         if (Serializer.INSTANCE != null) {
             EmoteCraftInstance.LOGGER.warn("Emotecraft is loaded multiple times, please load it only once!");
             return getConfigAs();
         }
 
-        Serializer.INSTANCE = new Serializer<>(new ConfigSerializer<>(configSuppler, version), configClass)/* {
-            @Override
-            protected Gson initializeSerializer(GsonBuilder builder) {
-                builder.registerTypeAdapter(Animation.class, FastAnimationSerializer.INSTANCE);
-                builder.registerTypeAdapterFactory(new CompletableFutureAdapterFactory());
-                return super.initializeSerializer(builder);
-            }
-        }*/;
-
+        Serializer.INSTANCE = new Serializer<>(new ConfigSerializer<>(configSuppler, version), configClass, configPath);
         return getConfigAs();
     }
 
